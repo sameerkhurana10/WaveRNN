46331a4f9ac896f2812865bc28fabdbd86766382
geneing <geneing@gmail.com>
2018-10-21 14:49:33 -0700

    Converted from python notebook

---
+++ hparams.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+import numpy as np
+
+# NOTE: If you want full control for model architecture. please take a look
+# at the code and change whatever you want. Some hyper parameters are hardcoded.
+
+# Default hyperparameters:
+hparams = tf.contrib.training.HParams(
+    name="WaveRNN",
+    bits=9,
+
+)
+
+
+def hparams_debug_string():
+    values = hparams.values()
+    hp = ['  %s: %s' % (name, values[name]) for name in sorted(values)]
+    return 'Hyperparameters:\n' + '\n'.join(hp)
---
+++ preprocess.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+"""
+Preprocess dataset for WaveRNN
+
+usage: preprocess.py [options] <name> <in_dir> <out_dir>
+
+options:
+    <in_dir>                 folder containing your training wavs
+    --num_workers=<n>        Num workers.
+    --hparams=<parmas>       Hyper parameters [default: ].
+    --preset=<json>          Path of preset parameters (json).
+    -h, --help               Show help message.
+"""
+from docopt import docopt
+import os
+from multiprocessing import cpu_count
+from tqdm import tqdm
+import importlib
+from hparams import hparams
+
+import matplotlib.pyplot as plt
+import math, pickle, os, glob
+import numpy as np
+from utils import *
+from utils.dsp import *
+
+
+def get_files(path, extension='.wav') :
+    filenames = []
+    for filename in glob.iglob(f'{path}/**/*{extension}', recursive=True):
+        filenames += [filename]
+    return filenames
+
+
+def convert_file(path):
+    wav = load_wav(path, encode=False)
+    mel = melspectrogram(wav)
+    quant = (wav + 1.) * (2**hparams.bits - 1) / 2
+    return mel.astype(np.float32), quant.astype(np.int)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    name = args["<name>"]
+    in_dir = args["<in_dir>"]
+    out_dir = args["<out_dir>"]
+    num_workers = args["--num_workers"]
+    num_workers = cpu_count() if num_workers is None else int(num_workers)
+    preset = args["--preset"]
+    if preset is not None:
+        with open(preset) as f:
+            hparams.parse_json(f.read())
+    # Override hyper parameters
+    hparams.parse(args["--hparams"])
+    assert hparams.name == "WaveRNN"
+
+    quant_path = os.path.join(out_dir, 'quant/')
+    mel_path = os.path.join(out_dir, 'mel/')
+    os.makedirs(quant_path, exist_ok=True)
+    os.makedirs(mel_path, exist_ok=True)
+
+    wav_files = get_files(in_dir)
+
+    # This will take a while depending on size of dataset
+    dataset_ids = []
+    for i, path in enumerate(wav_files) :
+        dataset_id = path.split('/')[-1][:-4]
+        dataset_ids += [dataset_id]
+        m, x = convert_file(path)
+        np.save(f'{mel_path}{dataset_id}.npy', m)
+        np.save(f'{quant_path}{dataset_id}.npy', x)
+        print('%i/%i'%(i + 1, len(wav_files)))
+    with open(out_dir + 'dataset_ids.pkl', 'wb') as f:
+        pickle.dump(dataset_ids, f)
